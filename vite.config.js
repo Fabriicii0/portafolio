74f6d1ab928f0d84288c67190391350f128e690f
Fabriicii0 <fabricioescobar623@gmail.com>
2024-03-13 16:32:48 -0600

    n

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import hbsContext  from './context/context';
 export default defineConfig(
     {
-        base: './portafolio/',
+        base: '/portafolio',
         appType: 'mpa',
         build: {
             rollupOptions: {
